53ef6e93c5924d1aabccc9f23258f7ffd1877221
Mariza Mariette Buntubwimana <mariza.mariette@sky.uk>
2024-03-01 23:33:48 +0000

    exercicios 3.1 e 3.2

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -20,18 +20,103 @@ Isso me deixa triste e cabisbaixo'''
 
 #a
 versos_poema = poema.split('/')
-for verso in versos_poema:
-    print(verso)
-print()
+#for verso in versos_poema:
+#   print(verso)
+#print()
 
 #b
 
 poema_completo = versos_poema + novos_versos.split("\n")
-for verso in poema_completo:
-    print(verso)
-print()
+#for verso in poema_completo:
+#   print(verso)
+#print()
 
-ultimos_versos = versos_poema_completo[-2:]
+#c
 
-for verso in ultimos_versos:
-    print(verso)
+#for verso in poema_completo[-2:]:
+#    print(verso)
+
+#d
+
+#num = 0
+#for verso in poema_completo:
+ #   if str(verso).count("samba") >= 1:
+ #       num = num + 1
+#print(num)
+
+#e
+
+#vogais = ["a", "e", "i", "o","u"]
+#for v in vogais:
+#    print(str(v) +": " + str(str(poema_completo).count(str(v))))
+
+#3.2
+#a
+disciplinaAnoPassado={
+  "AED": 15,
+  "POO": 20,
+  "IP":19
+}
+
+disciplinaEsteAno={
+    "CDSI": 17,
+    "Econ": 18,
+    "DIAM":14
+}
+#print(disciplinaAnoPassado, disciplinaEsteAno)
+
+#b
+disciplinaEsteAno.update(disciplinaAnoPassado)
+#print(disciplinaEsteAno)
+
+#c
+#print(disciplinaEsteAno.keys())
+
+#d
+#print(disciplinaEsteAno.values())
+
+#e
+#print(dict(sorted(disciplinaEsteAno.items())))
+
+
+class gestDiscipliinas:
+
+    #f
+    def exists( self, dictionary,myStrinng):
+        if myStrinng in dictionary:
+            print(discipline + " existe no dicionario")
+     #g
+    def valueGreaterThan(self,dictionay,myValue):
+        notasAltas = []
+        for key, nota in dictionay.items():
+            if myValue < nota:
+                notasAltas.append(key)
+        return notasAltas
+
+ #h Calcular a média das notas das disciplinas presentes no dicionário
+
+    def media(self,dictionary):
+        media = 0
+        for key,nota in dictionary.items():
+            media = media + nota
+        media = media/ len(dictionary)
+        return media
+
+#i Obter a lista das três disciplinas com as melhores notas
+    def high(self, dictionary):
+        notasMaisAltas = []
+        sorted(dictionary.items(), key=lambda x: x[1])
+        notasMaisAltas= sorted(dictionary.items(), key=lambda x: x[1])[-3:]
+        return notasMaisAltas
+        
+
+
+
+manage = gestDiscipliinas()
+discipline = "DIAM"
+#manage.exists(disciplinaEsteAno,discipline)
+nota = 17
+#print(manage.valueGreaterThan(disciplinaEsteAno,nota))
+#print(manage.media(disciplinaEsteAno))
+print(manage.high(disciplinaEsteAno))
+
