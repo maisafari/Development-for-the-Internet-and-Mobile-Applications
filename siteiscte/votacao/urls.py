d0b8256a67abd3c28ebc36035e7ea9c5a2cd801d
josevilamourac <josevilamoura@gmail.com>
2024-03-23 12:19:04 +0000

    new

--- siteiscte/votacao/urls.py
+++ siteiscte/votacao/urls.py
@@ -3,17 +3,13 @@ from . import views
 app_name = 'votacao'
 
 urlpatterns = [
-    # ex: votacao/
     path("", views.index, name='index'),
     # ex: votacao/1
     path("<int:questao_id>", views.detalhe,
          name='detalhe'),
-    # ex: votacao/3/resultados
     path('<int:questao_id>/resultados', views.resultados,
          name='resultados'),
-    # ex: votacao/5/voto
-    # ex: votacao/3/resultados
-    # ex: votacao/5/voto
+
     path('<int:questao_id>/voto', views.voto,
          name='voto'),
     path('votacao/criarquestao/', views.criar_questao, name='criarquestao'),
